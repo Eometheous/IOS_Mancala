b8ab664bc3dd12c24d8b92b2f1aa75f9e05468f2
Eometheous <jonathan.sthomas@icloud.com>
2023-11-20 11:57:13 -0800

    Initial Commit

---
+++ Mancala/MancalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MancalaApp.swift
+//  Mancala
+//
+//  Created by Jonathan Thomas on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MancalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
