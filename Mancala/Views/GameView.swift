278c95251b581b518a87c576d904145adb59fd93
Eometheous <jonathan.sthomas@icloud.com>
2023-11-27 09:53:43 -0800

    Added better spacing and set frame to fit the entire screen

--- Mancala/Views/GameView.swift
+++ Mancala/Views/GameView.swift
@@ -13,33 +13,52 @@ struct GameView: View {
     var body: some View {
         HStack {
             Text("\(model.get(position: 7))").font(.largeTitle)
+            Spacer()
             VStack {
                 Text("\(model.get(position: 6))").font(.largeTitle)
+                Spacer()
                 Text("\(model.get(position: 8))").font(.largeTitle)
             }
+            Spacer()
             VStack {
                 Text("\(model.get(position: 5))").font(.largeTitle)
+                Spacer()
                 Text("\(model.get(position: 9))").font(.largeTitle)
             }
+            Spacer()
             VStack {
                 Text("\(model.get(position: 4))").font(.largeTitle)
+                Spacer()
                 Text("\(model.get(position: 10))").font(.largeTitle)
             }
+            Spacer()
             VStack {
                 Text("\(model.get(position: 3))").font(.largeTitle)
+                Spacer()
                 Text("\(model.get(position: 11))").font(.largeTitle)
             }
+            Spacer()
             VStack {
                 Text("\(model.get(position: 2))").font(.largeTitle)
+                Spacer()
                 Text("\(model.get(position: 12))").font(.largeTitle)
             }
+            Spacer()
             VStack {
                 Text("\(model.get(position: 1))").font(.largeTitle)
+                Spacer()
                 Text("\(model.get(position: 13))").font(.largeTitle)
             }
+            Spacer()
             Text("\(model.get(position: 0))").font(.largeTitle)
         }
-        .padding()
+        .padding(100)
+        .frame(
+            minWidth: 0,
+            maxWidth: .infinity,
+            minHeight: 0,
+            maxHeight: .infinity
+        )
         .background(Color.board)
     }
 }
