ccc9d753843456debef439ba5e7f7fac0879342e
Eometheous <jonathan.sthomas@icloud.com>
2023-12-04 13:13:20 -0800

    added placing beads in pits functionality

--- Mancala/Models/MancalaGame.swift
+++ Mancala/Models/MancalaGame.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class MancalaGame {
-    var model: Model
+    var beads: Model
     
     var isPlayerBTurn = true
     var isGameStarted = false
@@ -17,12 +17,12 @@ class MancalaGame {
     let PLAYER_B_MANCALA = 7
     
     init(model: Model) {
-        self.model = model
+        self.beads = model
     }
     
     func pickupBeadsAt(pit: Int) {
-        var numberOfBeadsPickedUp = model.get(position: pit)
-        model.update(position: pit, newValue: 0)
+        let numberOfBeadsPickedUp = beads.get(position: pit)
+        beads.update(position: pit, newValue: 0)
         placeBeadsInPits(startingPit: pit, startingNumOfBeads: numberOfBeadsPickedUp)
     }
     
@@ -32,18 +32,21 @@ class MancalaGame {
         
         while numOfBeads > 0 {
             currentPit+=1
-            
-            if (currentPit == PLAYER_B_MANCALA && isPlayerBTurn) {
-                model.update(position: PLAYER_B_MANCALA, newValue: model.get(position: PLAYER_B_MANCALA) + 1)
-                numOfBeads-=1
+            if (currentPit == 14) {
+                currentPit = 0
             }
-            else if (currentPit == PLAYER_A_MANCALA && !isPlayerBTurn) {
-                model.update(position: PLAYER_A_MANCALA, newValue: model.get(position: PLAYER_A_MANCALA) + 1)
-                numOfBeads-=1
-            }
+//            if (currentPit == PLAYER_B_MANCALA && isPlayerBTurn) {
+//                beads.update(position: PLAYER_B_MANCALA, newValue: beads.get(position: PLAYER_B_MANCALA) + 1)
+//                numOfBeads-=1
+//            }
+//            else if (currentPit == PLAYER_A_MANCALA && !isPlayerBTurn) {
+//                beads.update(position: PLAYER_A_MANCALA, newValue: beads.get(position: PLAYER_A_MANCALA) + 1)
+//                numOfBeads-=1
+//            }
             if (numOfBeads > 0) {
-                model.update(position: currentPit, newValue: model.get(position: currentPit) + 1)
+                beads.update(position: currentPit, newValue: beads.get(position: currentPit) + 1)
             }
+            
             numOfBeads-=1
         }
     }
@@ -53,6 +56,10 @@ class MancalaGame {
     }
     
     func setPlayersTurn(playersTurn: Bool) {
-        isPlayerBTurn = playersTurn
+        isPlayerBTurn.toggle()
     }
+}
+
+extension MancalaGame {
+    static let sampleGame = MancalaGame(model: Model(numberOfStartingBeads: 4))
 }
--- Mancala/Views/GameView.swift
+++ Mancala/Views/GameView.swift
@@ -8,82 +8,170 @@
 import SwiftUI
 
 struct GameView: View {
-    var model: Model
+    var game: MancalaGame
+    
+    // this is very cringe, but it works :/
+    @State private var pit0 = MancalaGame.sampleGame.beads.get(position: 0)
+    @State private var pit1 = MancalaGame.sampleGame.beads.get(position: 1)
+    @State private var pit2 = MancalaGame.sampleGame.beads.get(position: 2)
+    @State private var pit3 = MancalaGame.sampleGame.beads.get(position: 3)
+    @State private var pit4 = MancalaGame.sampleGame.beads.get(position: 4)
+    @State private var pit5 = MancalaGame.sampleGame.beads.get(position: 5)
+    @State private var pit6 = MancalaGame.sampleGame.beads.get(position: 6)
+    @State private var pit7 = MancalaGame.sampleGame.beads.get(position: 7)
+    @State private var pit8 = MancalaGame.sampleGame.beads.get(position: 8)
+    @State private var pit9 = MancalaGame.sampleGame.beads.get(position: 9)
+    @State private var pit10 = MancalaGame.sampleGame.beads.get(position: 10)
+    @State private var pit11 = MancalaGame.sampleGame.beads.get(position: 11)
+    @State private var pit12 = MancalaGame.sampleGame.beads.get(position: 12)
+    @State private var pit13 = MancalaGame.sampleGame.beads.get(position: 13)
     
     var body: some View {
         HStack {
-            Text("\(model.get(position: 7))")
+            Text("\(pit7)")
                 .font(.largeTitle)
                 .padding(30)
                 .background(Color.pit)
             Spacer()
             VStack {
-                Text("\(model.get(position: 6))")
+                Text("\(pit6)")
                     .font(.largeTitle)
                     .padding(30)
                     .background(Color.pit)
-                Text("\(model.get(position: 8))")
+                    .onTapGesture {
+                        if (game.beads.get(position: 6) != 0) {
+                            game.pickupBeadsAt(pit: 6)
+                            updateView()
+                        }
+                    }
+                Text("\(pit8)")
                     .font(.largeTitle)
                     .padding(30)
                     .background(Color.pit)
+                    .onTapGesture {
+                        if (game.beads.get(position: 8) != 0) {
+                            game.pickupBeadsAt(pit: 8)
+                            updateView()
+                        }
+                    }
             }
             Spacer()
             VStack {
-                Text("\(model.get(position: 5))")
+                Text("\(pit5)")
                     .font(.largeTitle)
                     .padding(30)
                     .background(Color.pit)
-                Text("\(model.get(position: 9))")
+                    .onTapGesture {
+                        if (game.beads.get(position: 5) != 0) {
+                            game.pickupBeadsAt(pit: 5)
+                            updateView()
+                        }
+                    }
+                Text("\(pit9)")
                     .font(.largeTitle)
                     .padding(30)
                     .background(Color.pit)
+                    .onTapGesture {
+                        if (game.beads.get(position: 9) != 0) {
+                            game.pickupBeadsAt(pit: 9)
+                            updateView()
+                        }
+                    }
             }
             Spacer()
             VStack {
-                Text("\(model.get(position: 4))")
+                Text("\(pit4)")
                     .font(.largeTitle)
                     .padding(30)
                     .background(Color.pit)
-                Text("\(model.get(position: 10))")
+                    .onTapGesture {
+                        if (game.beads.get(position: 4) != 0) {
+                            game.pickupBeadsAt(pit: 4)
+                            updateView()
+                        }
+                    }
+                Text("\(pit10)")
                     .font(.largeTitle)
                     .padding(30)
                     .background(Color.pit)
+                    .onTapGesture {
+                        if (game.beads.get(position: 10) != 0) {
+                            game.pickupBeadsAt(pit: 10)
+                            updateView()
+                        }
+                    }
             }
             Spacer()
             VStack {
-                Text("\(model.get(position: 3))")
+                Text("\(pit3)")
                     .font(.largeTitle)
                     .padding(30)
                     .background(Color.pit)
-                Text("\(model.get(position: 11))")
+                    .onTapGesture {
+                        if (game.beads.get(position: 3) != 0) {
+                            game.pickupBeadsAt(pit: 3)
+                            updateView()
+                        }
+                    }
+                Text("\(pit11)")
                     .font(.largeTitle)
                     .padding(30)
                     .background(Color.pit)
+                    .onTapGesture {
+                        if (game.beads.get(position: 11) != 0) {
+                            game.pickupBeadsAt(pit: 11)
+                            updateView()
+                        }
+                    }
             }
             Spacer()
             VStack {
-                Text("\(model.get(position: 2))")
+                Text("\(pit2)")
                     .font(.largeTitle)
                     .padding(30)
                     .background(Color.pit)
-                Text("\(model.get(position: 12))")
+                    .onTapGesture {
+                        if (game.beads.get(position: 2) != 0) {
+                            game.pickupBeadsAt(pit: 2)
+                            updateView()
+                        }
+                    }
+                Text("\(pit12)")
                     .font(.largeTitle)
                     .padding(30)
                     .background(Color.pit)
+                    .onTapGesture {
+                        if (game.beads.get(position: 12) != 0) {
+                            game.pickupBeadsAt(pit: 12)
+                            updateView()
+                        }
+                    }
             }
             Spacer()
             VStack {
-                Text("\(model.get(position: 1))")
+                Text("\(pit1)")
                     .font(.largeTitle)
                     .padding(30)
                     .background(Color.pit)
-                Text("\(model.get(position: 13))")
+                    .onTapGesture {
+                        if (game.beads.get(position: 1) != 0) {
+                            game.pickupBeadsAt(pit: 1)
+                            updateView()
+                        }
+                    }
+                Text("\(pit13)")
                     .font(.largeTitle)
                     .padding(30)
                     .background(Color.pit)
+                    .onTapGesture {
+                        if (game.beads.get(position: 13) != 0) {
+                            game.pickupBeadsAt(pit: 13)
+                            updateView()
+                        }
+                    }
             }
             Spacer()
-            Text("\(model.get(position: 0))")
+            Text("\(game.beads.get(position: 0))")
                 .font(.largeTitle)
                 .padding(30)
                 .background(Color.pit)
@@ -97,8 +185,27 @@ struct GameView: View {
         )
         .background(Color.board)
     }
+    
+    func updateView() {
+        pit0 = game.beads.get(position: 0)
+        pit1 = game.beads.get(position: 1)
+        pit2 = game.beads.get(position: 2)
+        pit3 = game.beads.get(position: 3)
+        pit4 = game.beads.get(position: 4)
+        pit5 = game.beads.get(position: 5)
+        pit6 = game.beads.get(position: 6)
+        pit7 = game.beads.get(position: 7)
+        pit8 = game.beads.get(position: 8)
+        pit9 = game.beads.get(position: 9)
+        pit10 = game.beads.get(position: 10)
+        pit11 = game.beads.get(position: 11)
+        pit12 = game.beads.get(position: 12)
+        pit13 = game.beads.get(position: 13)
+    }
 }
 
 #Preview {
-    GameView(model: Model.sampleData)
+//    GameView(game: .constant(MancalaGame.sampleGame))
+    GameView(game: MancalaGame.sampleGame)
+//    GameView(beadsModel: Model.sampleData, game: MancalaGame.sampleGame)
 }
